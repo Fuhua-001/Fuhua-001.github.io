9549ffd5051f1dd23a17601a104a4506b8bd30d0
Fuhua-001 <yatta001x@gmail.com>
2026-07-10 11:53:49 +0700

    Fix Netlify function routing basePath

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 const serverless = require('serverless-http');
 const app = require('../../server'); // import the express app
 
-// Wrap the Express app
-module.exports.handler = serverless(app);
+// Wrap the Express app with base path for Netlify Functions
+module.exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
