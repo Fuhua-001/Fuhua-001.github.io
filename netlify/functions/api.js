34b47ba531791155b0eb4ff10a02b041fcbda4e4
Fuhua-001 <yatta001x@gmail.com>
2026-07-14 16:13:17 +0700

    chore: Remove Netlify configuration

--- netlify/functions/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../server'); // import the express app
-
-// Wrap the Express app with base path for Netlify Functions
-module.exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
