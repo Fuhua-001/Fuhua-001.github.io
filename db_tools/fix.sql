3d52aa6af91483dbb555e2dd4e35014708dfe9cd
Fuhua-001 <yatta001x@gmail.com>
2026-07-20 15:00:23 +0700

    Cleanup unused files and update DB comments

--- db_tools/fix.sql
+++
@@ -1,5 +0,0 @@
-USE quotation_db;
-ALTER TABLE customers 
-MODIFY sales_remark TEXT COMMENT 'Sales Remark', 
-MODIFY category VARCHAR(50) COMMENT 'Category',
-MODIFY level_group VARCHAR(50) COMMENT 'ชื่อกลุ่มระดับลูกค้า/ผู้ขาย';
